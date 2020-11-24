9e5648719f308e38345a9b5cdfaa894e4d34bac9
mudamoon97 <53881669+mudamoon97@users.noreply.github.com>
2020-11-24 16:59:54 +0100

    Javascript grade

---
+++ index.js
@@ -0,0 +1,29 @@
+var msg = 'enter the grade of student:';
+
+var grade = prompt(msg);
+
+
+if (grade >=99){
+    console.log('Distinction')
+}else if(grade >=89 ){
+    console.log('Exellent')
+}else if (grade >= 70){
+    console.log('Very Good')
+}else if(grade >=60){
+    console.log('Good')
+}else if(grade >=50){
+    console.log('Average')
+}else if(grade >= 49){
+    console.log('Pass')
+}else if(grade >=40){
+    console.log('Fail')
+
+}else
+{
+    console.log('Input not accepted')
+}
+
+
+
+
+   
